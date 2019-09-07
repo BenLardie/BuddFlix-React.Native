5e645ab2c14c8deb2869b2bd6ff08223e376fcb9
Ben Lardie <benlardie@gmail.com>
2019-09-06 22:58:50 -0400

    we got a trailer

--- Trailer.js
+++ Trailer.js
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, WebView } from 'react-native';
+import { StyleSheet, WebView, View } from 'react-native';
 import { useNavigation } from 'react-navigation-hooks';
 import axios from 'axios';
 
 
 const Trailer = ( movieId ) => {
-    console.log(movieId)
-    const key = '446d9b88173473ed0acd5d7fed14558e';
+    
+    const ApiKey = '446d9b88173473ed0acd5d7fed14558e';
     const [trailerId, setTrailerId] = useState();
 
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'space-between',
+  
+    },
+    video: {
+      marginTop: 20,
+      maxHeight: 200,
+      width: 320,
+      flex: 1
+    }
+  });
+
     useEffect(() => {
-        const url = `https://api.themoviedb.org/3/movie/${movieId.movieId}?api_key=${key}&append_to_response=videos`
+        const url = `https://api.themoviedb.org/3/movie/${movieId.movieId}?api_key=${ApiKey}&append_to_response=videos`
             axios.get(url).then(response => {
             if (response.data.videos.results.length > 0) {
             const results = `https://www.youtube.com/embed/`+ response.data.videos.results[0].key
@@ -23,7 +39,12 @@ const Trailer = ( movieId ) => {
     },[movieId])
 
     return (
-        <WebView source={{uri: trailerId }} />
+        <View style={styles.container}>
+        <WebView 
+            source={{uri: trailerId }} 
+            style={styles.video}
+        />
+        </View>
     )
 };
 
